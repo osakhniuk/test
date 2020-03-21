a96bdf7824c8192c76aa10506a8de051fcae6779
osakhniuk <osakhniuk@datagrok.ai>
2020-03-21 19:04:59 +0200

    1241

--- Covid/package.js
+++ Covid/package.js
@@ -3,24 +3,10 @@ class CovidPackage extends GrokPackage {
     //tags: app
     //name: Covid19
     startApp(context) {
-        let emptyTable = DataFrame.create();
-        let view = grok.addTableView(emptyTable);
-        view.name = 'Covid-19';
-        view.basePath = '';
-        view.root.className = 'grok-view grok-table-view';
-
-        function update() {
-            ui.setUpdateIndicator(view.root, true);
-            grok.callQuery('Covid:CasesByCountries', true, 100).then(fc => {
-                let data = JSON.parse(fc.getParamValue('stringResult'))['data'];
-            view.dataFrame = data !== null ? CovidPackage.dataToTable(data, 'covid') : emptyTable;
-            ui.setUpdateIndicator(view.root, false);
-        });
-        }
-
-        update();
-
-
+        grok.query('Covid:CasesByCountries', {}).then((products) => {
+            var customersView = null;
+        grok.addTableView(products);
+    });
 
 
     }
